6fdbede86a2e2f7a1445ea946544e4f206603e8a
Kenneth Waegeman <kenneth.waegeman@ugent.be>
2021-01-15 13:15:28 +0100

    pass storage_name

--- lib/vsc/administration/sync.py
+++ lib/vsc/administration/sync.py
@@ -37,7 +37,7 @@ STORAGE_PRJ_LIMIT_WARNING = 1
 STORAGE_PRJ_LIMIT_CRITICAL = 2
 
 #TODO: vsc-config tier1c names
-VSC_TIER1C_SCRATCH = 'dodrioscratch'
+VSC_TIER1C_STORAGE = ['dodrioscratch']
 
 
 class VscTier1cSync(Sync):
@@ -48,22 +48,19 @@ class VscTier1cSync(Sync):
     def process_t1_users(self, account_ids, quotas, storage_name, dry_run=False):
         """
         Process the users.
-        If all users are part of a project, and only have actual data/directories inside projects
-        this method should be integraded with process_t1_projects
+        Note: If all users are part of a project, and only have actual data/directories inside projects
+        this method should not be used and swill be done by process_projects members method
 
         """
         error_users = []
         ok_users = []
 
-        if storage_name != VSC_TIER1C_SCRATCH:
-            log.raiseException("%s is not a storage name for tier1c" % storage_name)
-
         for vsc_id in sorted(account_ids):
-            user = VscTier1cUser(vsc_id, rest_client=self.apc, use_user_cache=True, dry_run=dry_run)
 
             try:
-                user.create_home_dir()
-                user.populate_home_dir()
+                user = VscTier1cUser(vsc_id, storage_name=storage_name,
+                    rest_client=self.apc, use_user_cache=True, dry_run=dry_run)
+                user.prepare_dirs()
                 update_user_status(user, self.apc)
 
                 ok_users.append(user)
@@ -75,9 +72,10 @@ class VscTier1cSync(Sync):
         error_quota = []
 
         for quota in quotas:
-            user = VscTier1cUser(quota.user, rest_client=self.apc, use_user_cache=True, dry_run=dry_run)
 
             try:
+                user = VscTier1cUser(quota.user, storage_name=storage_name,
+                        rest_client=self.apc, use_user_cache=True, dry_run=dry_run)
                 user.set_quota()
                 ok_quota.append(quota)
             except Exception:
@@ -98,19 +96,17 @@ class VscTier1cSync(Sync):
         ok_prj = MonoidDict(copy.deepcopy(listm))
         error_prj = MonoidDict(copy.deepcopy(listm))
 
-        if storage_name != VSC_TIER1C_SCRATCH:
-            log.raiseException("%s is not a storage name for tier1c" % storage_name)
-
         for prj_id in sorted(prj_ids):
 
-            prj = VscTier1cProject(prj_id, rest_client=self.apc, dry_run=dry_run)
+            prj = VscTier1cProject(prj_id, storage_name=storage_name, rest_client=self.apc, dry_run=dry_run)
 
             try:
-                prj.create_scratch_fileset()
-                prj.set_scratch_quota()
+                prj.create_fileset()
+                prj.set_quota()
 
                 modified_member_list = prj.modified_members(datestamp)
                 factory = lambda pid: VscTier1cUser(pid,
+                                                    storage_name=storage_name,
                                                     rest_client=self.apc,
                                                     use_user_cache=True,
                                                     dry_run=dry_run)
@@ -118,8 +114,8 @@ class VscTier1cSync(Sync):
 
                 for member in modified_members:
                     try:
-                        prj.set_member_scratch_quota(member)  # half of the project quota
-                        prj.create_member_scratch_dir(member)
+                        prj.set_member_quota(member)  # half of the project quota
+                        prj.create_member_dir(member)
 
                         ok_prj[prj.prj_id] = [member.account.vsc_id]
                     except Exception:
@@ -146,7 +142,6 @@ class VscTier1cSync(Sync):
         stats = {}
         #FIXME: decide these below and move to vsc-config stuff
         institute = 'GhentTier1c'
-        storage_name = VSC_TIER1C_SCRATCH
         project_prefix = 'proj_'
 
         (users_ok, users_fail) = ([], [])
@@ -157,52 +152,54 @@ class VscTier1cSync(Sync):
             #FIXME: parse accounts for tier1?
             log.info("Found %d %s accounts that have changed in the accountpage since %s" %
                         (len(changed_accounts), institute, self.start_timestamp))
+            for storage_name in VSC_TIER1C_STORAGE:
+                storage_changed_quota = self.get_user_storage_quota(storage_name=storage_name)
+                log.info("Found %d quota changes on storage %s in the accountpage",
+                    len(storage_changed_quota), storage_name)
 
-            storage_changed_quota = self.get_user_storage_quota(storage_name=storage_name)
-            log.info("Found %d quota changes on storage %s in the accountpage",
-                len(storage_changed_quota), storage_name)
-
-            (users_ok, users_fail, quota_ok, quota_fail) = self.process_t1_users(
-                changed_accounts,
-                storage_changed_quota,
-                storage_name,
-                dry_run=dry_run)
-            stats["%s_users_sync" % (storage_name,)] = len(users_ok)
-            stats["%s_users_sync_fail" % (storage_name,)] = len(users_fail)
-            stats["%s_users_sync_fail_warning" % (storage_name,)] = STORAGE_USERS_LIMIT_WARNING
-            stats["%s_users_sync_fail_critical" % (storage_name,)] = STORAGE_USERS_LIMIT_CRITICAL
-            stats["%s_quota_sync" % (storage_name,)] = len(quota_ok)
-            stats["%s_quota_sync_fail" % (storage_name,)] = len(quota_fail)
-            stats["%s_quota_sync_fail_warning" % (storage_name,)] = STORAGE_QUOTA_LIMIT_WARNING
-            stats["%s_quota_sync_fail_critical" % (storage_name,)] = STORAGE_QUOTA_LIMIT_CRITICAL
+                (users_ok, users_fail, quota_ok, quota_fail) = self.process_t1_users(
+                    changed_accounts,
+                    storage_changed_quota,
+                    storage_name,
+                    dry_run=dry_run)
+                stats["%s_users_sync" % (storage_name,)] = len(users_ok)
+                stats["%s_users_sync_fail" % (storage_name,)] = len(users_fail)
+                stats["%s_users_sync_fail_warning" % (storage_name,)] = STORAGE_USERS_LIMIT_WARNING
+                stats["%s_users_sync_fail_critical" % (storage_name,)] = STORAGE_USERS_LIMIT_CRITICAL
+                stats["%s_quota_sync" % (storage_name,)] = len(quota_ok)
+                stats["%s_quota_sync_fail" % (storage_name,)] = len(quota_fail)
+                stats["%s_quota_sync_fail_warning" % (storage_name,)] = STORAGE_QUOTA_LIMIT_WARNING
+                stats["%s_quota_sync_fail_critical" % (storage_name,)] = STORAGE_QUOTA_LIMIT_CRITICAL
 
         (prj_ok, prj_fail) = ([], [])
         if self.options.projects:
             #FIXME: How exactly to fetch tier1c projects from AP? see as seperate site or parse attribute
             changed_groups, _ = self.get_groups(site=institute)
             changed_prjs = [g for g in changed_groups if g.vsc_id.startswith(project_prefix)]
-            #FIXME: can we use vo logic of accountpage below for projects?
-            changed_prj_quota = [q for q in self.get_vo_storage_quota(storage_name=storage_name)
-                if q.fileset.startswith(project_prefix)]
-
-            #FIXME: virtual_organisation -> project in AP?
-            prjs = sorted(set([v.vsc_id for v in changed_prjs] + [v.virtual_organisation for v in changed_prj_quota]))
-
             log.info("Found %d %s projects that have changed in the accountpage since %s" %
                         (len(changed_prjs), institute, self.start_timestamp))
-            log.info("Found %d %s VOs that have changed quota in the accountpage since %s" %
-                        (len(changed_prj_quota), institute, self.start_timestamp))
-            log.debug("Found the following {institute} projects: {prjs}".format(institute=institute, prjs=prjs))
 
-            (prj_ok, prj_fail) = self.process_projects(
-                prjs,
-                storage_name,
-                self.start_timestamp,
-                dry_run=dry_run)
-            stats["%s_prj_sync" % (storage_name,)] = len(prj_ok)
-            stats["%s_prj_sync_fail" % (storage_name,)] = len(prj_fail)
-            stats["%s_prj_sync_fail_warning" % (storage_name,)] = STORAGE_PRJ_LIMIT_WARNING
-            stats["%s_prj_sync_fail_critical" % (storage_name,)] = STORAGE_PRJ_LIMIT_CRITICAL
+            for storage_name in VSC_TIER1C_STORAGE:
+                #FIXME: can we use vo logic of accountpage below for projects?
+                changed_prj_quota = [q for q in self.get_vo_storage_quota(storage_name=storage_name)
+                    if q.fileset.startswith(project_prefix)]
+
+                #FIXME: virtual_organisation -> project in AP?
+                prjs = sorted(set([v.vsc_id for v in changed_prjs] + [v.virtual_organisation for v in changed_prj_quota]))
+
+                log.info("Found %d %s projects that have changed quota in the accountpage since %s" %
+                        (len(changed_prj_quota), institute, self.start_timestamp))
+                log.debug("Found the following {institute} projects: {prjs}".format(institute=institute, prjs=prjs))
+
+                (prj_ok, prj_fail) = self.process_projects(
+                    prjs,
+                    storage_name,
+                    self.start_timestamp,
+                    dry_run=dry_run)
+                stats["%s_prj_sync" % (storage_name,)] = len(prj_ok)
+                stats["%s_prj_sync_fail" % (storage_name,)] = len(prj_fail)
+                stats["%s_prj_sync_fail_warning" % (storage_name,)] = STORAGE_PRJ_LIMIT_WARNING
+                stats["%s_prj_sync_fail_critical" % (storage_name,)] = STORAGE_PRJ_LIMIT_CRITICAL
 
         self.thresholds = stats
 
