01110f62cfe8bda037ac5f220d7091928ce84432
Kenneth Waegeman <kenneth.waegeman@ugent.be>
2021-01-20 13:05:46 +0100

    implement quota functions

--- lib/vsc/administration/tier1c.py
+++ lib/vsc/administration/tier1c.py
@@ -23,7 +23,8 @@ import pwd
 from vsc.administration.user import VscAccountPageUser
 from vsc.administration.vo import VscAccountPageVo
 from vsc.utils import fancylogger
-from vsc.config.base import GENT, VSC, VscStorage
+from vsc.config.base import GENT, VSC, VscStorage, Storage
+from vsc.accountpage.wrappers import mkVscAccount
 
 from vsc.filesystem.lustre import LustreOperations
 from vsc.filesystem.gpfs import GpfsOperations
@@ -40,7 +41,7 @@ class Dodrio(Storage):
     def __init__(self):
         super().__init__(
                 filesystem='dodrioscratch',
-                # Two new fields without hard dependency on gpfs
+                # Two new fields without hard dependency on gpfs, FIXME others still need values
                 fsbackend='lustre',
                 mount_point='/dodrio/scratch',
                 #metadata_replication_factor=2
@@ -51,10 +52,6 @@ class Dodrio(Storage):
                 #user_grouping_fileset=1
                 #user_path=/arcanine/scratch
                 )
-
-
-
-
 
 class VscStorageBackend(object):
     """
@@ -87,8 +84,11 @@ class VscStorageBackend(object):
         (path, _) = self.path_templates[template_type](name)
         return os.path.join(self.storage.mount_point, path)
 
-    def _create_fileset(self, path, name, mode=0o770):
+    def _get_project_path(self, pjname):
+        return self._get_path(PROJ, pjname)
 
+    def _create_fileset(self, path, name, mode=0o770):
+        """ create a fileset on storage """
         if not self.fsops.get_fileset_info(self.storage_name, name):
             log.info("Creating new fileset on %s with name %s and path %s",
                 self.storage_name, name, path)
@@ -96,21 +96,33 @@ class VscStorageBackend(object):
             self.fsops.make_dir(base_dir_hierarchy)
             self.fsops.make_fileset(path, name)
         else:
-            log.info("Fileset %s already exists for %s ... not creating again." % name)
+            log.info("Fileset %s already exists for %s ... not creating again.", name, self.storage_name)
 
         self.fsops.chmod(mode, path)
 
     def create_project_fileset(self, pjname, moderator_id, group_owner_id):
-        path = self._get_path(PROJ, pjname)
+        """ create a fileset for a project on storage """
+        path = self._get_project_path(pjname)
         self._create_fileset(path, pjname)
         self.fsops.chown(moderator_id, group_owner_id, path)
 
-    def _set_quota(self, path, name, quota):
+    def _set_fileset_quota(self, path, name, quota):
+        """ Set quota on a fileset on storage """
+        try:
+            # expressed in bytes, retrieved in KiB from the backend
+            hard = quota * 1024 * self.storage.data_replication_factor
+            soft = int(hard * self.vsc.quota_soft_fraction)
 
-        pass
+            self.fsops.set_fileset_quota(soft, path, name, hard)
+            self.fsops.set_fileset_grace(path, self.vsc.vo_storage_grace_time)
+
+        except Exception:
+            log.raiseException("Unable to set quota on path %s", path)
+
 
     def set_project_quota(self, pjname, quota=None):
-        path = self._get_path(PROJ, pjname)
+        """ set project quota on storage """
+        path = self._get_project_path(pjname)
         if not quota:
             quota = self.storage.quota_prj
             log.error("No %s quota information available for %s", pjname, self.storage_name)
@@ -120,7 +132,21 @@ class VscStorageBackend(object):
             quota = quota.hard
 
         log.info("Setting %s quota on storage %s to %d", pjname, self.storage_name, quota)
-        self._set_quota(path, pjname, quota)
+        self._set_fileset_quota(path, pjname, quota)
+
+    def _create_member_dir(self, target, uid, gid, mode=0o700, override_permissions=False):
+        """Create a member-owned directory in the fileset."""
+        self.fsops.create_stat_directory(
+            target,
+            mode,
+            int(uid),
+            int(gid),
+            override_permissions)
+
+    def create_project_member_dir(self, pjname, member, muid, mgid):
+        target = os.path.join(self._get_project_path(pjname), member)
+        self._create_member_dir(target, muid, mgid)
+
 
 class VscTier1cUser(VscAccountPageUser):
     '''VSC  Tier1c AccountPage User with Lustre Backend '''
@@ -143,7 +169,7 @@ class VscTier1cUser(VscAccountPageUser):
             we could create 'projects' for users but that's ugly """
         return
 
-    #FIXME: implement functions to have home on scratch?
+    #FIXME: implement more functions(homeonscratch,..) ?
 
 # FIXME: reuse VO logic or use a VscAccountPageProject class/accountpage implementation
 class VscTier1cProject(VscAccountPageVo):
@@ -159,7 +185,8 @@ class VscTier1cProject(VscAccountPageVo):
 
         self.storage_name = storage_name
 
-        self.backend = VscStorageBackend(storage=storage, path_templates=path_templates, dry_run=dry_run)
+        self.backend = VscStorageBackend(storage=storage, storage_name=storage_name,
+                        path_templates=path_templates, dry_run=dry_run)
 
         super().__init__(pjid, rest_client=None)
         self.pjid = self.vo_id
@@ -173,6 +200,12 @@ class VscTier1cProject(VscAccountPageVo):
 
         return self._quota_cache
 
+    @property
+    def prj(self):
+        """ prj in AP which currently uses vo logic """
+        #FIXME: how to handle projects from AP
+        return self.vo
+
     def project_path(self, pjid, host_institute):
         """ Path templates for tier1c. Can be integraded with vsc-config """
         return (os.path.join(host_institute, PROJECT_SUBDIR, pjid), None)
@@ -183,14 +216,14 @@ class VscTier1cProject(VscAccountPageVo):
         """ Create the project scratch fileset """
 
         try:
-            moderator = mkVscAccount(self.rest_client.account[self.vo.moderators[0]].get()[1])
-            fileset_group_owner_id = self.vo.vsc_id_number
+            moderator = mkVscAccount(self.rest_client.account[self.prj.moderators[0]].get()[1])
+            fileset_group_owner_id = self.prj.vsc_id_number
             fileset_owner_id = moderator.vsc_id_number
         except HTTPError:
             log.exception("Cannot obtain moderator information from account page, setting ownership to nobody")
             fileset_owner_id = pwd.getpwnam('nobody').pw_uid
         except IndexError:
-            log.error("There is no moderator available for %s", self.vo.vsc_id)
+            log.error("There is no moderator available for %s", self.prj.vsc_id)
             fileset_owner_id = pwd.getpwnam('nobody').pw_uid
         try:
             self.backend.create_project_fileset(self.pjid, fileset_owner_id, fileset_group_owner_id)
@@ -208,14 +241,15 @@ class VscTier1cProject(VscAccountPageVo):
 
     def modified_members(self, datestamp):
         """ Get a list of modified members of this project """
-        #TODO: projects in accountpage?
-        return self.rest_client.vo[self.pjid].member.modified[datestamp].get()
+        return self.prj.member.modified[datestamp].get()
 
-    def create_member_scratch_dir(self, member):
-        """ Create a directory for a member in the project """
-        pass
+    def create_member_dir(self, member):
+        """Create a directory in the fileset that is owned
+        by the member with name <PROJECT>/<vscid>."""
+        self.backend.create_project_member_dir(self, self.pjid, member.user_id,
+                member.account.vsc_id_number, member.usergroup.vsc_id_number)
+
 
     def set_member_scratch_quota(self, member):
         """ Note: we can't do this (yet) on Lustre, we only can set global user quota """
         return
-
